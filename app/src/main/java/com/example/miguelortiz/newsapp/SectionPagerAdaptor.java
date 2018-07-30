1a3b1f40fd8520973a1528b72b08bf11565ff809
Miguel Ortiz <maov_98@yahoo.com>
2018-07-29 21:43:04 -0500

    Added the fragment management pieces

--- app/src/main/java/com/example/miguelortiz/newsapp/SectionPagerAdaptor.java
+++ app/src/main/java/com/example/miguelortiz/newsapp/SectionPagerAdaptor.java
@@ -5,13 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SectionPagerAdaptor extends FragmentPagerAdapter {
 
-    private List<Fragment> mFragmentList = new ArrayList<>();
-    private List<String> mFragmentTitleList = new ArrayList<>();
     private Context context;
     private String technologyTab;
     private String politicsTab;
@@ -74,10 +70,6 @@ public class SectionPagerAdaptor extends FragmentPagerAdapter {
         return new noConnectionFragment();
 
         }
-
-
-
-
 
         return null;
     }
